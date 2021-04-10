a4e0e3f50c679f0781a5334b0ad8eb223fe49345
jeriet123 <jerietr@gmail.com>
2021-04-09 19:09:22 -0500

    created a map of the nuclear explosions

--- Nuclear Explosion Data Practice script.R
+++ Nuclear Explosion Data Practice script.R
@@ -1,7 +1,5 @@
 ## Loads the Nuclear Explosion dataset from github
-nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/
-                                      tidytuesday/master/data/2019/2019-08-20/
-                                      nuclear_explosions.csv")
+nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-20/nuclear_explosions.csv")
 
 ## Loads the packages ggplot2, lubridate, tidyr, and stringr.
 library(ggplot2)
@@ -12,4 +10,18 @@ library(stringr)
 ## Converts the data in the variable date_long into POSIXT via the format year, month,
 ## and day
 nuclear_explosions$date_long <- ymd(nuclear_explosions$date_long)
-variable.names(nuclear_explosions)
+
+# Stores all of the variable names under Variables
+Variables <- variable.names(nuclear_explosions)
+
+# Stores all of the sources of nuclear explosions and the countries and regions 
+# where nuclear explosions occurred. 
+All_Countries <- unique(nuclear_explosions$country)
+All_Regions   <- unique(nuclear_explosions$region)
+All_Sources   <- unique(nuclear_explosions$source)
+
+# Stores the names, the different type, and the different purposes of the nukes.
+Purposes         <- unique(nuclear_explosions$purpose)
+Types            <- unique(nuclear_explosions$type)
+Identified_Nukes <- unique(nuclear_explosions$name)
+
